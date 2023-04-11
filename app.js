5dab6b9b0502041c3db23386c6495c34f210a20e
Stephen Jones <stephen-jones@hotmail.co.uk>
2023-04-11 18:28:15 +0100

    design page added

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
-const portfolioTiles = document.querySelectorAll('.portfolio-tiles');
-tilesImage = document.querySelectorAll('.tiles-img');
-const tilesText = document.querySelectorAll('.tiles-text-container');
-const burgerNav = document.querySelector('.nav-burger');
-
+const portfolioTiles = document.querySelectorAll(".portfolio-tiles");
+tilesImage = document.querySelectorAll(".tiles-img");
+const tilesText = document.querySelectorAll(".tiles-text-container");
+const burgerNav = document.querySelector(".nav-burger");
 
 const toggleNav = () => {
-    const header = document.querySelector('.header');
-    header.classList.toggle('side-nav');
-}
+  const header = document.querySelector(".header");
+  header.classList.toggle("side-nav");
+};
 
-/*
-function hoverOver(index) {
-    console.log('hovered');
-    tilesImage[index].classList.add('tiles-img-active');
-    tilesText[index].classList.add('tiles-text-container-active');
-}
-
-function hoverLeave(index) {
-    console.log('off')
-    tilesImage[index].classList.remove('tiles-img-active');
-    tilesText[index].classList.remove('tiles-text-container-active');
-
-}
-
-portfolioTiles.forEach((element, index) => {
-    element.addEventListener('mouseover',() => hoverOver(index));
-    element.addEventListener('mouseleave', () => hoverLeave(index));
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".fas")) {
+    toggleNav();
+  }
 });
-*/
-
-document.addEventListener('click', (e) => {
-    if(e.target.matches('.fas')) {
-        toggleNav()
-        
-
-    }
-})
